cea5e348936c75aac6d1412b27d18c8c075356be
DESKTOP-HBCRLE2\asus <auriwaan0046@gmail.com>
2017-05-11 00:16:30 +0700

    no message

--- stl_stanless.sql
+++ stl_stanless.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 10, 2017 at 04:58 PM
+-- Generation Time: May 10, 2017 at 05:14 PM
 -- Server version: 5.7.14
 -- PHP Version: 5.6.25
 
