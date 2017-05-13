c61c6f3a3b1f62466595b7b57dcee0536cad0183
DESKTOP-HBCRLE2\asus <auriwaan0046@gmail.com>
2017-05-13 23:21:50 +0700

    no message

--- report/connect.php
+++
@@ -1,4 +0,0 @@
-<?php
-	$conn= mysqli_connect("localhost","root","","stl") or die("Error: " . mysqli_error($con));
-	mysqli_query($conn, "SET NAMES 'utf8' "); 
-?>
--- report/connectt.php
+++
@@ -1,4 +0,0 @@
-<?php
-$mysqli=mysqli_connect("localhost","root","","stl");
-$mysqli->set_charset("utf8");
-?>
